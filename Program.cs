036089d0b52320549442755c501af175705e5f0f
Denis Gabdullin <denis.gabdullin@yandex.ru>
2022-01-25 17:38:10 +0300

    Add array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите строки, символы через запятую: ");
+string words = Console.ReadLine();
+string[] array = words.Split(',');
+string[] newarray = array;
+
+void printarray(string[] newarray)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(array[position] + " ");
+        position++;
+    }
+}
+printarray(newarray);
