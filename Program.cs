a84ca7c056d72dec65a845a7e0d3457be61d310a
Denis Gabdullin <denis.gabdullin@yandex.ru>
2022-01-25 22:04:22 +0300

    complete program

--- Program.cs
+++ Program.cs
@@ -1,16 +1,37 @@
-﻿Console.Write("Введите строки, символы через запятую: ");
+﻿Console.Write("Введите символы через запятую: ");
 string words = Console.ReadLine();
-string[] array = words.Split(',');
-string[] newarray = array;
-
-void printarray(string[] newarray)
+string[] array = words.Split(','); //задаем исходный массив элементов без запятой.
+int index = 0;
+for (int i = 0; i < array.Length; i++) //узнаем, сколько необходимо создать элементов в новом массиве.
 {
-    int count = array.Length;
+    if (array[i].Length <= 3)
+    {
+        index++;
+    }
+}
+string[] newarray = new string[index]; //создаем новый массив с необходимым числом элементов.
+void sort(string[] col)// метод заполнения нового массива нужными нам элементами.
+{
+    int length = array.Length;
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[n] = array[i];
+            n++;
+        }
+    }
+}
+void printarray(string[] arr)// печатаем массив
+{
+    int count = newarray.Length;
     int position = 0;
     while (position < count)
     {
-        Console.WriteLine(array[position] + " ");
+        Console.Write(newarray[position] + " ");
         position++;
     }
 }
+sort(newarray);
 printarray(newarray);
